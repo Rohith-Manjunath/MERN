68067469f70a58a1b42b5209f2439b7b1c0d82ac
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2023-07-29 04:21:40 +0530

    Initiated React setup

---
+++ backend/db/ProductsSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  ImageUrl: {
+    type: String,
+    required: true,
+  },
+  Model: {
+    type: String,
+    required: true,
+  },
+  Price: {
+    type: Number,
+    required: true,
+  },
+  Description: {
+    type: String,
+    required: true,
+  },
+});
+
+const ProductModel = mongoose.model("product", userSchema);
+module.exports = ProductModel;
