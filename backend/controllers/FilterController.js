a13ebebe18e879edd51372012888d612e701fcde
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2023-11-17 01:43:26 +0530

    feat: add price sorting filters
    This commit introduces new features to enable users to sort products by price.
    - Implemented filters for low to high and high to low price sorting.
    - Enhances user experience by providing flexible product viewing options.

--- backend/controllers/FilterController.js
+++ backend/controllers/FilterController.js
@@ -9,6 +9,7 @@ const Filter1 = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
 const Filter2 = async (req, res) => {
   try {
     let filter = await Product.find({ Price: { $gte: 21000, $lte: 40000 } });
@@ -18,6 +19,7 @@ const Filter2 = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
 const Filter3 = async (req, res) => {
   try {
     let filter = await Product.find({ Price: { $gt: 40000 } });
@@ -28,4 +30,26 @@ const Filter3 = async (req, res) => {
   }
 };
 
-module.exports = { Filter1, Filter2, Filter3 };
+// New filter for sorting by price in increasing order
+const Filter4 = async (req, res) => {
+  try {
+    let filter = await Product.find().sort({ Price: 1 });
+    res.json(filter);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// New filter for sorting by price in decreasing order
+const Filter5 = async (req, res) => {
+  try {
+    let filter = await Product.find().sort({ Price: -1 });
+    res.json(filter);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { Filter1, Filter2, Filter3, Filter4, Filter5 };
--- backend/routes/FilterRoutes.js
+++ backend/routes/FilterRoutes.js
@@ -4,6 +4,8 @@ const {
   Filter1,
   Filter2,
   Filter3,
+  Filter4,
+  Filter5,
 } = require("../controllers/FilterController");
 
 router.get("/filter1", Filter1);
@@ -11,5 +13,7 @@ router.get("/filter1", Filter1);
 router.get("/filter2", Filter2);
 
 router.get("/filter3", Filter3);
+router.get("/filter4", Filter4);
+router.get("/filter5", Filter5);
 
 module.exports = router;
